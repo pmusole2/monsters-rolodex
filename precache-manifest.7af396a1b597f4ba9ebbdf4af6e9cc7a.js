d61a911d1d26619216598cefb2853e7a6fae0f11
pmusole2 <pmusole2@gmail.com>
2020-03-24 18:05:05 +0200

    Updates

---
+++ precache-manifest.7af396a1b597f4ba9ebbdf4af6e9cc7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35c7ad0a9a06b6ed65acfddf6dfdca9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "db495edc716b2d77b681",
+    "url": "/monsters-rolodex/static/css/main.cb3671e9.chunk.css"
+  },
+  {
+    "revision": "63df8db3c2f25d2b1573",
+    "url": "/monsters-rolodex/static/js/2.3f827674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3f827674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db495edc716b2d77b681",
+    "url": "/monsters-rolodex/static/js/main.a750ccc3.chunk.js"
+  },
+  {
+    "revision": "f46a4c3c1c2f680ebf61",
+    "url": "/monsters-rolodex/static/js/runtime-main.082acf91.js"
+  }
+]);
